c637f1c220414d6943445e74bf11171fd299b40b
Philip Ramsey <philip@point5solutions.com>
2015-07-13 19:59:15 -0700

    cleaning up setup.py; removing scaffolding boilerplate

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
-import os
-
 from setuptools import setup, find_packages
-
-here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.txt')) as f:
-    README = f.read()
-with open(os.path.join(here, 'CHANGES.txt')) as f:
-    CHANGES = f.read()
 
 requires = [
     'pyramid',
@@ -24,17 +16,9 @@ requires = [
 setup(name='whereikeepinfo',
       version='0.0.1',
       description='whereikeepinfo',
-      long_description=README + '\n\n' + CHANGES,
-      classifiers=[
-        "Programming Language :: Python",
-        "Framework :: Pyramid",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        ],
       author='Philip Ramsey',
       author_email='philip@whereikeep.info',
-      url='',
-      keywords='web wsgi bfg pylons pyramid',
+      url='http://whereikeep.info',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
